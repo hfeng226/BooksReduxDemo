3ad924e9b0cf4930dd4e5e22edca0229bf1f54c0
Helen Feng <hfeng226@gmail.com>
2017-10-06 15:59:01 -0700

    wip

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+	return [
+	{title: 'Javascript: The Good Parts', pages: 101},
+	{title: 'The Last Lecture', pages: 25},
+	{title: 'Influence', pages: 50},
+	{title: 'The Power of Habit', pages:60}
+	];
+}
